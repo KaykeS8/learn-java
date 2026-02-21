202d74abe3f809b7f318358e30cac82a65153a08
Kayke simao <kayke.py@gmail.com>
2026-02-21 11:57:57 -0300

    Gerando medias de votação a partir da estrutura map

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,28 +1,43 @@
 package app;
 
-import entities.Product;
-
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-        Map<Product, Integer> stock = new HashMap<>();
+        Scanner sc = new Scanner(System.in);
 
-        Product p1 = new Product("TV", 1600.0);
-        Product p2 = new Product("Tablet", 900.0);
-        Product p3 = new Product("Smartphone", 3499.0);
+        System.out.print("Enter file full path: ");
+        String file = sc.nextLine();
+        Map<String, Integer> votes = new LinkedHashMap<>();
 
-        stock.put(p1, 20);
-        stock.put(p2, 8);
-        stock.put(p3, 14);
+        try (sc; BufferedReader bf = new BufferedReader(new FileReader(file))) {
+            String line = bf.readLine();
+            while (line != null)  {
 
-        System.out.println(stock);
+                String[] fields = line.split(",");
+                String name = fields[0];
+                int count = Integer.parseInt(fields[1]);
 
-        Product ps = new Product("TV", 1600.0);
-        System.out.println("Contains 'ps' key: " + stock.containsKey(ps));
-        System.out.println(stock);
+                if (votes.containsKey(name)) {
+                    int votesSoFar = votes.get(name);
+                    votes.put(name, count + votesSoFar);
+                } else {
+                    votes.put(name, count);
+                }
+                line = bf.readLine();
+            }
 
+            for (String key : votes.keySet()) {
+                System.out.println(key + ": " + votes.get(key));
+            }
 
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
